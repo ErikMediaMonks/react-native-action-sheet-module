10b96f042a0db7b3f0405f54f210daf4ad6cfe55
Erik Poort <erik@mediamonks.com>
2017-08-03 23:01:09 -0300

    Add initial iOS files

---
+++ ios/RNActionSheetModule/RNActionSheetModule.h
@@ -0,0 +1,14 @@
+//
+//  RNActionSheetModule.h
+//  RNActionSheetModule
+//
+//  Created by Erik Poort on 03/08/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RNActionSheetModule : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNActionSheetModule/RNActionSheetModule.m
@@ -0,0 +1,13 @@
+//
+//  RNActionSheetModule.m
+//  RNActionSheetModule
+//
+//  Created by Erik Poort on 03/08/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import "RNActionSheetModule.h"
+
+@implementation RNActionSheetModule
+
+@end
